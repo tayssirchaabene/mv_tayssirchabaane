22844ca2bc3b164c4eef0c17c743bb0d321e314a
tayssir chabaane <tayssirchabaane60@gmail.com>
2024-04-22 11:42:03 +0200

    Querying the Database

--- src/Controller/VinylController.php
+++ src/Controller/VinylController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\VinylMixRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -26,13 +26,14 @@ class VinylController extends AbstractController
         ]);
     }
 
-    #[Route("/browse/{slug}", name: 'app_browse')]
-    function browse(string $slug = null): Response
+    #[Route('/browse/{slug}', name: 'app_browse')]
+    public function browse(VinylMixRepository $mixRepository, string $slug = null): Response
     {
-
-        $genre = $slug ? u(str_replace("-", " ", $slug))->title(true) : null;
+        $genre = $slug ? u(str_replace('-', ' ', $slug))->title(true) : null;
+        $mixes = $mixRepository->findAll();
         return $this->render('vinyl/browse.html.twig', [
             'genre' => $genre,
+            'mixes' => $mixes,
         ]);
     }
 }
