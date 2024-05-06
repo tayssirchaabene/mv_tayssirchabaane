6dfba0c6665a22070278e79feba24579ec9c26bd
tayssir chabaane <tayssirchabaane60@gmail.com>
2024-05-06 10:43:52 +0200

    Updating Flash Message & Rich vs Anemic Modelsan Entity

--- src/Controller/MixController.php
+++ src/Controller/MixController.php
@@ -47,9 +47,9 @@ class MixController extends AbstractController
     {
         $direction = $request->request->get('direction', 'up');
         if ($direction === 'up') {
-            $mix->setVotes($mix->getVotes() + 1);
+            $mix->upVote();
         } else {
-            $mix->setVotes($mix->getVotes() - 1);
+            $mix->downVote();
         }
         $entityManager->flush();
         return $this->redirectToRoute('app_mix_show', [
--- src/Entity/VinylMix.php
+++ src/Entity/VinylMix.php
@@ -77,7 +77,14 @@ class VinylMix
         return $this;
     }
 
-    
+    public function upVote(): void
+    {
+        $this->votes++;
+    }
+    public function downVote(): void
+    {
+        $this->votes--;
+    }
 
     public function getDescription(): ?string
     {
